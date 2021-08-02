f9c95e325a9494b1c3f27413fe0459d13e3993a5
deepaakhil84 <deepaakhiltvm@gmail.com>
2021-08-02 23:58:08 +0100

    basket service

---
+++ src/app/basket.service.ts
@@ -0,0 +1,16 @@
+import { getNumberOfCurrencyDigits } from '@angular/common';
+import { Injectable } from '@angular/core';
+import { Restaurents } from './models/restaurent.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BasketService {
+  restaurentMenu$: Restaurents[] | undefined;
+  constructor() {}
+
+  getMenu(menu: Restaurents[]) {
+    this.restaurentMenu$ = menu;
+    console.log('basket service', this.restaurentMenu$);
+  }
+}
